ad19a7aec17e887475b3d4ded2b7041c858cf318
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2016-02-21 19:54:39 -0500

    use express to server page and for heroku compatibility

--- server.js
+++ server.js
@@ -1,60 +1,35 @@
-var http = require('http');
-var url = require('url');
-var path = require('path');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var PORT = process.env.PORT || 3000;
 
-var server = http.createServer(handleRequest);
-server.listen(8080);
+app.use(express.static(__dirname));
+server.listen(PORT);
 
-console.log('Server started on port 8080');
+console.log('Server started on port ' + PORT);
 
-function handleRequest(req, res) {
-  var pathname = req.url;
-  
-  if (pathname == '/') {
-    pathname = '/index.html';
-  }
-  
-  var ext = path.extname(pathname);
-
-  var typeExt = {
-    '.html': 'text/html',
-    '.js':   'text/javascript',
-    '.css':  'text/css'
-  };
-
-  var contentType = typeExt[ext] || 'text/plain';
-
-  fs.readFile(__dirname + pathname,
-    function (err, data) {
-      if (err) {
-        res.writeHead(500);
-        return res.end('Error loading ' + pathname);
-      }
-      res.writeHead(200,{ 'Content-Type': contentType });
-      res.end(data);
-    }
-  );
-}
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
 
 var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
    socket.on('mouse', function (data) {
      console.log("Received x: " + data.x + ", y: " + data.y + ", red: " + data.r +
         ", blue: " + data.b + ", green: " + data.g + ", alpha: " + data.a);
-     socket.broadcast.emit('mouse', data); 
+     socket.broadcast.emit('mouse', data);
    });
-   
+
    socket.on('background', function (data) {
-    console.log("Received r: " + data.r + ", g: " + data.g + ", b: " + data.b + ", alpha: " + data.a);  
+    console.log("Received r: " + data.r + ", g: " + data.g + ", b: " + data.b + ", alpha: " + data.a);
     socket.broadcast.emit('background', data);
    });
-   
+
    socket.on('clear', function (data) {
-    console.log("Received r: " + data.r + ", g: " + data.g + ", b: " + data.b + ", alpha: " + data.a);  
+    console.log("Received r: " + data.r + ", g: " + data.g + ", b: " + data.b + ", alpha: " + data.a);
     socket.broadcast.emit('clear', data);
    });
-   
+
    socket.on('disconnect', function () {
        console.log("Client has disconnected");
    });
